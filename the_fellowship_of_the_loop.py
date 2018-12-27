981c36961ebb95987eb4810492cf840f16060686
francesca mangialardo <44606486+mangiafrangette@users.noreply.github.com>
2018-12-27 16:59:16 +0100

    do_coupling: typo strenght > strength

--- the_fellowship_of_the_loop.py
+++ the_fellowship_of_the_loop.py
@@ -1,9 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright (c) 2018,
-# Sebastian Barzaghi <sebastian.barzaghi@studio.unibo.it>,
-# Martina Dello Buono <martina.dellobuono@studio.unibo.it>,
-# Fabio Mariani <fabio.mariani6@studio.unibo.it>,
-# Silvio Peroni <silvio.peroni@unibo.it>
+# Copyright (c) 2018, Silvio Peroni <essepuntato@gmail.com>
 #
 # Permission to use, copy, modify, and/or distribute this software for any purpose
 # with or without fee is hereby granted, provided that the above copyright notice
@@ -16,11 +12,12 @@
 # DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS
 # ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS
 # SOFTWARE.
-
-# The following importing line is used to include in the definition of this class
-# the particular functions implemented by a group. The 'my_test_group' module specified
-# here is just a placeholder, since it defines only the signature of the various
-# functions but it returns always None.
+#
+#
+# This file is just a stub of the particular module that every group should
+# implement for making its project work. In fact, all these functions returns None,
+# which is not compliant at all with the specifications that have been provided at
+# https://comp-think.github.io/2018-2019/slides/14%20-%20Project.html
 
 import csv
 from csv import DictReader
@@ -52,14 +49,14 @@ def do_citation_graph(data, sse): #spiegazione nel commit così non si sporca il
     return cit_graph
 
 def do_coupling(data, sse, doi_1, doi_2):
-    coupling_strenght = 0
+    coupling_strength = 0
     coupling_list = []
     for dict in data:
         if doi_1 == dict["doi"] or doi_2 == dict["doi"] and dict["known refs"]:
             coupling_list.extend(dict["known refs"].split("; "))
     coupling_set = set(coupling_list)
-    coupling_strenght = len(coupling_list) - len(coupling_set)
-    return coupling_strenght
+    coupling_strength = len(coupling_list) - len(coupling_set)
+    return coupling_strength
 
 def do_aut_coupling(data, sse, aut_1, aut_2):
     return None
