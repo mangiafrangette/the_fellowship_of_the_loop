2e69ba7b7c1db28156ebea0cb885ec12318c5862
francesca mangialardo <44606486+mangiafrangette@users.noreply.github.com>
2018-12-28 18:41:06 +0100

    aggiunta ricerchina2 (utilizzata in do_aut_coupling)

--- ancillary_functions.py
+++ ancillary_functions.py
@@ -2,3 +2,8 @@ def ricerchina(sse, doi_to_search):
     for dict in sse.data:
         if doi_to_search == dict["doi"]:
             return dict
+
+def ricerchina2(data, doi_to_search):
+    for dict in data:
+        if doi_to_search == dict["doi"]:
+            return dict
