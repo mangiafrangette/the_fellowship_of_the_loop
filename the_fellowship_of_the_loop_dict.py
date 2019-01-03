2c991c6a63e09ac7d88ae411fb30d991fa17f747
federicafaithbologna <45122707+federicafaithbologna@users.noreply.github.com>
2019-01-03 20:02:25 +0100

    nuovo con data = dict di dict

---
+++ the_fellowship_of_the_loop_dict.py
@@ -0,0 +1,12 @@
+import csv
+from collections import Counter
+from networkx import nx, MultiDiGraph, Graph
+from ancillary_functions import *
+
+def process_citation_data(file_path):
+    with open(file_path, 'r', encoding='utf-8') as csvfile:
+        reader = csv.DictReader(csvfile)
+        data = defaultdict(dict)
+        for row in reader:
+            data[row["doi"]] = dict(row)
+    return data
