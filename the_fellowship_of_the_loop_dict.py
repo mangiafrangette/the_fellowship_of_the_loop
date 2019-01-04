de77c1ddc38d22e14989b01670cc7c3b7f775e08
francesca mangialardo <44606486+mangiafrangette@users.noreply.github.com>
2019-01-04 12:57:39 +0100

    aggiunto do_citation_graph

--- the_fellowship_of_the_loop_dict.py
+++ the_fellowship_of_the_loop_dict.py
@@ -10,3 +10,17 @@ def process_citation_data(file_path):
         for row in reader:
             data[row["doi"]] = dict(row)
     return data
+
+def do_citation_graph(data, sse):
+    cit_graph = DiGraph()
+
+    for doi, dict in data.items():
+        if dict["known refs"]:
+            searched_doi = [ricerchina(sse, dict["doi"])]
+            current_pretty_node = sse.pretty_print(searched_doi)[0]
+
+            for ref in dict["known refs"].split("; "):
+                searched_ref = [ricerchina(sse, ref)]
+                pretty_ref_node = sse.pretty_print(searched_ref)[0]
+                cit_graph.add_edge(current_pretty_node, pretty_ref_node)
+    return cit_graph
