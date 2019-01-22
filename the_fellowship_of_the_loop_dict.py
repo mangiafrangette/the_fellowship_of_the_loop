d80aedde25be896da276a0302af141962aa775a5
Carlo Teo Pedretti <41264663+friendlynihilist@users.noreply.github.com>
2019-01-22 11:32:42 +0100

    updated to manage all "split" cases in known refs

--- the_fellowship_of_the_loop_dict.py
+++ the_fellowship_of_the_loop_dict.py
@@ -33,6 +33,7 @@ def process_citation_data(file_path):
         for row in reader:  # iteration over "rows" in .csv file
             data[row["doi"]] = dict(row)  # the keys of the macrodict are values contained under "doi" column. their values are subdictionaries.
             data[row["doi"]]["known refs"] = set(data[row["doi"]]["known refs"].split("; ")).difference({""})
+        print(data)
     return data
 
 
@@ -44,7 +45,7 @@ def do_citation_graph(data, sse):
             searched_doi = [meta_search(sse, dict["doi"])]  # must be a list with one element because of pretty print
             current_pretty_node = sse.pretty_print(searched_doi)[0]  # [0] is to access the list
 
-            for ref in dict["known refs"].split("; "):
+            for ref in dict["known refs"]:
                 searched_ref = [meta_search(sse, ref)]  # same search but with the doi present in known refs
                 pretty_ref_node = sse.pretty_print(searched_ref)[0]
                 cit_graph.add_edge(current_pretty_node, pretty_ref_node)  # actual creation of the graph.
@@ -56,9 +57,9 @@ def do_coupling(data, sse, doi_1, doi_2):
     coupling_strength = 0
     if doi_1 in data and doi_2 in data:  # added in case the searched doi are not in data.
         if data[doi_1]["known refs"]:
-            coupling_list.extend(data[doi_1]["known refs"].split("; "))  # add all the dois contained in known refs to coupling_list
+            coupling_list.extend(data[doi_1]["known refs"])  # add all the dois contained in known refs to coupling_list
         if data[doi_2]["known refs"]:
-            coupling_list.extend(data[doi_2]["known refs"].split("; "))  # same as above
+            coupling_list.extend(data[doi_2]["known refs"])  # same as above
         coupling_set = set(coupling_list)
         coupling_strength = len(coupling_list) - len(coupling_set)
         return coupling_strength
@@ -73,9 +74,9 @@ def do_aut_coupling(data, sse, aut_1, aut_2):
         if data[dict_sse["doi"]]["known refs"]:  # with the previous iteration we found the doi in meta to use for accessing the macrodict's value "known refs"
             aut_split = dict_sse["authors"].split("; ")
             if aut_1 in aut_split and aut_2 not in aut_split:  # it allows to avoid coauthorships
-                aut_1_coupling_set.update(data[dict_sse["doi"]]["known refs"].split("; "))
+                aut_1_coupling_set.update(data[dict_sse["doi"]]["known refs"])
             if aut_2 in aut_split and aut_1 not in aut_split:
-                aut_2_coupling_set.update(data[dict_sse["doi"]]["known refs"].split("; "))
+                aut_2_coupling_set.update(data[dict_sse["doi"]]["known refs"])
     intersection = aut_1_coupling_set.intersection(aut_2_coupling_set)  # intersection finds common DOIs between two sets
     aut_coupling_strength = len(intersection)
     return aut_coupling_strength
